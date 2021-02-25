6590e9b289c6f9fe3a2ca23f0d939441ec6a1f09
nicolaironning <nicolai.ronning@gmail.com>
2021-02-25 06:30:54 +0100

    new

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6d05ee9cdf1f2552eff63f930639c89c.js"
+  "/precache-manifest.cd3df03da549ad897a374e163df774ee.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "webapp"});
